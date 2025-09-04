a524e92950e5421d4f5b3e80d38b10661880411f
mlardelli <markus@LAPTOP-D1HVNL1G.localdomain>
2025-09-04 17:27:15 +0930

    added files for algorithms

---
+++ OnePlusOneEA.py
@@ -0,0 +1,6 @@
+"""
+Implementation of the (1+1) EA Algorithm as defined in the Week 3 Lecture
+"""
+
+class OnePlusOneEA:
+    pass
---
+++ RLS.py
@@ -0,0 +1,6 @@
+"""
+Implementation of the RLS Algorithm as defined in the Week 3 Lecture
+"""
+
+class RLS:
+    pass
