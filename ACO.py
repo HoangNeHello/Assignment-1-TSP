0c9c18c33131742f507b781ae6d828f684db7d60
Jiahe-SHI <a1808696@adelaide.edu.au>
2025-10-06 17:12:07 +1030

    Fix some error

--- ACO.py
+++ ACO.py
@@ -4,7 +4,7 @@ def onemax(bitstring):
     return sum(bitstring)
 
 class ACO:
-    def __init__(self, n=20, num_ants=10, r=0.1, q=1.0, max_iter=200, alpha=1.0, beta=1.0):
+    def __init__(self, n=100, num_ants=10, r=0.1, q=1.0, max_iter=100000, alpha=1.0, beta=1.0):
         '''
         n is the length of the bit string, 
         num_ants is the number of ants, 
@@ -22,7 +22,7 @@ class ACO:
         self.max_iter = max_iter
         self.alpha = alpha
         self.beta = beta
-        self.pheromone = [1.0] * n
+        self.pheromone = [0.5] * n
         self.best_solution = None
         self.best_fitness = float("-inf")
 
@@ -45,24 +45,25 @@ class ACO:
         for i in range(self.n):
             self.pheromone[i] *= (1 - self.r)
 
-        for i, bit in enumerate(solutions):
-            if bit == 1:
-                self.pheromone[i] += self.q * fitnesses / self.n
+        for sol, fit in zip(solutions, fitnesses):
+            for i, bit in enumerate(sol):
+                if bit == 1:
+                    self.pheromone[i] += self.q * fit / self.n
 
     def run(self, fitness_function):
+        best_curve = []
         for i in range(1, self.max_iter + 1):
-            solutions = []
-            fitnesses = []
+            solutions, fitnesses = [], []
             for j in range(self.num_ants):
                 s = self.solution()
                 f = fitness_function(s)
                 solutions.append(s)
                 fitnesses.append(f)
-
                 if f > self.best_fitness:
                     self.best_fitness = f
                     self.best_solution = s[:]
 
             self.update_pheromones(solutions, fitnesses)
+            best_curve.append(self.best_fitness)
 
-        return self.best_solution, self.best_fitness
+        return self.best_solution, self.best_fitness, best_curve
