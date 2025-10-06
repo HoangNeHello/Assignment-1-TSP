3a25370db3947aa392bd219cb5a96ac03a2b2e12
mlardelli <a1860596@adelaide.edu.au>
2025-10-06 21:59:12 +1030

    added (1+1) EA algorithm (logger is buggy, working on fix)

--- OnePlusOneEA.py
+++ OnePlusOneEA.py
@@ -6,13 +6,11 @@ from ioh import logger
 import sys
 import numpy as np
 
-def OnePlusOneEA(func):
-    for i in range(10):
-        pass
+import random
 
-# Please replace this `random search` by your `genetic algorithm`.
-def random_search(func, budget = None):
-    # budget of each run: 50n^2
+def OnePlusOneEA(func, budget = None):
+    # budget for each run (number of iterations) = 100,000
+    # set a default if budget isn't given, in this case 50n^2 (same as random_search function)
     if budget is None:
         budget = int(func.meta_data.n_variables * func.meta_data.n_variables * 50)
 
@@ -21,44 +19,65 @@ def random_search(func, budget = None):
     else:
         optimum = func.optimum.y
     print(optimum)
-    # 10 independent runs for each algorithm on each problem.
-    for r in range(10):
+
+    # Run 10 independant trials of the algorithm
+    f_opt = None
+    sdash_opt = None
+    for i in range(10):
         f_opt = sys.float_info.min
-        x_opt = None
-        for i in range(budget):
-            x = np.random.randint(2, size = func.meta_data.n_variables)
-            f = func(x)
-            if f > f_opt:
+        sdash_opt = None
+
+        # Run algorithm for a number of iterations given by budget
+        for j in range(budget):
+
+            # Get current problem value
+            sdash = func.state.current_best.x
+
+            # Iterate through all bits of sdash
+            for k in range(len(sdash)):
+
+                # random() creates a float 0.0 <= x < 1.0
+                # Checks if this value is less than 1/len(sdash)
+                # Results in the bitflip only happening with a probability of 1/len(sdash)
+                if (random.random() < 1/len(sdash)):
+                    # Performs a bitflip with boolean operators
+                    sdash[k] = 1 - sdash[k]
+                # print(k)
+
+            f = func(sdash)
+            if (f > f_opt):
                 f_opt = f
-                x_opt = x
-            if f_opt >= optimum:
+                sdash_opt = sdash
+            if (f_opt >= optimum):
                 break
+        
+        print(func.state)
         func.reset()
-    return f_opt, x_opt
+
+    return f_opt, sdash_opt
 
 # Declaration of problems to be tested.
-om = get_problem(fid = 1, dimension=50, instance=1, problem_class = ProblemClass.PBO)
-lo = get_problem(fid = 2, dimension=50, instance=1, problem_class = ProblemClass.PBO)
-labs = get_problem(fid = 18, dimension=50, instance=1, problem_class = ProblemClass.PBO)
+om = get_problem(fid = 1, dimension=100, instance=1, problem_class = ProblemClass.PBO)
+lo = get_problem(fid = 2, dimension=100, instance=1, problem_class = ProblemClass.PBO)
+labs = get_problem(fid = 18, dimension=100, instance=1, problem_class = ProblemClass.PBO)
 
-print(om.meta_data.name)
 # Create default logger compatible with IOHanalyzer
 # `root` indicates where the output files are stored.
 # `folder_name` is the name of the folder containing all output. You should compress this folder and upload it to IOHanalyzer
-l = logger.Analyzer(root="data", 
-    folder_name="run", 
-    algorithm_name="random_search", 
-    algorithm_info="test of IOHexperimenter in python")
+log = logger.Analyzer(root="data", 
+    folder_name="OnePlusOneEA_run", 
+    algorithm_name="OnePlusOneEA", 
+    algorithm_info="Implementation of the (1+1) EA in Python")
 
 
-# om.attach_logger(l)
-# random_search(om)
+om.attach_logger(log)
+OnePlusOneEA(om, 100000)
 
-# lo.attach_logger(l)
-# random_search(lo)
+lo.attach_logger(log)
+OnePlusOneEA(om, 100000)
 
-# labs.attach_logger(l)
-# random_search(labs)
+labs.attach_logger(log)
+OnePlusOneEA(om, 100000)
 
 # This statemenet is necessary in case data is not flushed yet.
-del l
+del log
