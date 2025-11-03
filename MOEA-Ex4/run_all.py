df83876dfc3aa8aabaf82561332d5ca5a29b0672
mlardelli <a1860596@adelaide.edu.au>
2025-11-03 16:27:16 +1030

    added new MOEA test data under ioh_data-1. f2100 to f2202 are complete, f2203 is unfinished

--- MOEA-Ex4/run_all.py
+++ MOEA-Ex4/run_all.py
@@ -1,9 +1,9 @@
 from subprocess import Popen
 
 procs = [
-    Popen(["python", "MOEA-10.py"]),
-    Popen(["python", "MOEA-20.py"]),
-    Popen(["python", "MOEA-50.py"]),
+    Popen(["python3", "MOEA-10.py"]),
+    Popen(["python3", "MOEA-20.py"]),
+    Popen(["python3", "MOEA-50.py"]),
 ]
 for p in procs:
     p.wait()
